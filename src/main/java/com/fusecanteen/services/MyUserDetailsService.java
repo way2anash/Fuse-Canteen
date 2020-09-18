ba1646d8ce9608f05d7dc669b7481cb03ecb7025
way2anash <ansarianash.np@gmail.com>
2020-09-18 09:14:13 +0545

    adding loggers to MyUserDetailsService

--- src/main/java/com/fusecanteen/services/MyUserDetailsService.java
+++ src/main/java/com/fusecanteen/services/MyUserDetailsService.java
@@ -2,6 +2,8 @@ package com.fusecanteen.services;
 
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -19,9 +21,12 @@ public class MyUserDetailsService implements UserDetailsService{
 	@Autowired
 	private UserService userService;
 
+	private final Logger LOG = LoggerFactory.getLogger(FoodService.class);
+	
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		
+		LOG.info("Getting user with email: {}.", email);
 		FuseUser user = userService.findUserByEmail(email);
 		
 		if(user == null) {
