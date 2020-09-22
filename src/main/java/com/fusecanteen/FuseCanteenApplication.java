33ff5c337fba1a59870f3ed8b301f452ed341784
way2anash <ansarianash.np@gmail.com>
2020-09-22 08:22:42 +0545

    Enabling Swagger 2

--- src/main/java/com/fusecanteen/FuseCanteenApplication.java
+++ src/main/java/com/fusecanteen/FuseCanteenApplication.java
@@ -3,7 +3,10 @@ package com.fusecanteen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class FuseCanteenApplication {
 
 	public static void main(String[] args) {
